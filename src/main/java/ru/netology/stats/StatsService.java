c4d1b13c045fc4fbaeaafd6d4dec18f778f42f0e
yuliyaDanzy <maksushina@mail.ru>
2023-11-04 09:47:08 +0700

    HomeworkMassivDone

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -8,14 +8,59 @@ public class StatsService {
         }
         return salesSum;
     }
+
     public long averageSum(long[] sales) {
         long averSum = 0;
-        for (int i =0; i < sales.length; i++) {
+        for (int i = 0; i < sales.length; i++) {
             averSum = averSum + sales[i];
 
         }
         return averSum / sales.length;
     }
 
-}
+    public int maxSales(int[] sales) {
+        int maxMonth = 0; // номер месяца с максимальными продажами среди просмотренных ранее
 
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[maxMonth] <= sales[i]) { // значит, в рассматриваемом i-м месяце продаж больше
+                maxMonth = i; // запомним его как максимальный
+            }
+        }
+
+        return maxMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
+    }
+
+    public int minSales(int[] sales) {
+        int minMonth = 0; // номер месяца с минимальными продажами среди просмотренных ранее
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[minMonth]) { // значит, в рассматриваемом i-м месяце продаж меньше
+                minMonth = i; // запомним его как минимальный
+            }
+        }
+
+        return minMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
+    }
+
+    public int averageSumDown(long[] sales) {
+        int averSumDown = 0;
+        long averSum = averageSum(sales);
+        for (long sale : sales) {
+            if (sale < averSum) {
+                averSumDown++;
+            }
+          }
+        return averSumDown;
+    }
+
+    public int averageSumUp(long[] sales) {
+        int averSumUp = 0;
+        long averSum = averageSum(sales);
+        for (long sale : sales) {
+            if (sale > averSum) {
+                averSumUp++;
+            }
+        }
+        return averSumUp;
+    }
+}
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -27,6 +27,45 @@ class StatsServiceTest {
         Assertions.assertEquals(expected, actual);
 
     }
+    @Test
+    public void testMaxMonth() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int actual = service.maxSales(sales);
+        int expected = 8;
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void testMinMonth() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int actual = service.minSales(sales);
+        int expected = 9;
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void averageSumDown() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.averageSumDown(sales);
+        long expected = 5;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void averageSumUp() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.averageSumDown(sales);
+        long expected = 5;
+        Assertions.assertEquals(expected, actual);
+    }
 
 }
 
